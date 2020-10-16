d2a9805dfba4dc15ae859a817b56b8b86ea3132a
Alex <alex.andrul10@gmail.com>
2020-10-16 18:05:56 +0100

    change navbar color

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,24 +12,29 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer
           style={{
-            // marginTop: `2rem`,
             position: `fixed`,
             bottom: `0`,
             width: `100%`,
             height: `60px`,
             paddingTop: `20px`,
             color: "black",
+            zIndex: `2000`,
           }}
         >
-          <p
-            style={{
-              color: "black",
-              marginLeft: `1rem`,
-            }}
-          >
-            {" "}
-            © {new Date().getFullYear()} BAE
-          </p>
+          <Location>
+            {({ location }) => (
+              <p
+                style={{
+                  color: `${
+                    location.pathname === "/" ? "rgb(249, 252, 66)" : "black"
+                  }`,
+                  marginLeft: `1rem`,
+                }}
+              >
+                © {new Date().getFullYear()} BAE
+              </p>
+            )}
+          </Location>
         </footer>
       </div>
     </>
