3f75cb69743d792434892515b5ebecfb9d08f1d8
Alex <alex.andrul10@gmail.com>
2020-10-28 20:54:50 +0000

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,36 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Form from "react-bootstrap/Form"
+import { Container, Button } from "react-bootstrap"
 import "../components/Styles/contact.css"
 const Contact = () => {
   return (
     <Layout>
       <SEO title="Contact" />
       <Container className="contact-container">
-        <Row>
-          <Col>Contact</Col>
-        </Row>
+        <Form className="contact-form">
+          <Form.Group>
+            <Form.Label>Nume</Form.Label>
+            <Form.Control type="text" placeholder="Nume" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email </Form.Label>
+            <Form.Control type="email" placeholder="Email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Mesaj</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <Button type="submit" className="contact-button">
+            Submit
+          </Button>
+        </Form>
+        <div className="contact-rectangle"></div>
       </Container>
     </Layout>
   )
