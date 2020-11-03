29afc959599d26d91365d4c7a2209aaeda0ed725
Alex <alex.andrul10@gmail.com>
2020-11-03 21:51:51 +0000

    change style

--- src/components/Templates/LucrariButtons.js
+++ src/components/Templates/LucrariButtons.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { Link } from "gatsby"
-import "../Styles/LucrariButtons.css"
+import "../Styles/lucrariButtons.css"
 
 const LucrariButtons = () => {
   return (
