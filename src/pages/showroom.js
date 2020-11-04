953a494b48d9915ab33554cb8e09ec9ff0a5ec56
Alex <alex.andrul10@gmail.com>
2020-11-04 15:38:51 +0000

    add showroom content

--- src/pages/showroom.js
+++ src/pages/showroom.js
@@ -1,19 +1,87 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-import { Container } from "react-bootstrap"
+import Modal from "react-bootstrap/Modal"
+import { TweenMax } from "gsap"
+import { Container, Col } from "react-bootstrap"
 import "../components/Styles/showroom.css"
 
-const Showroom = () => {
+const Showroom = ({ data }) => {
+  const [image, setImage] = useState({})
+  const [showImageModal, setShowImageModal] = useState(false)
+  const handleClose = () => setShowImageModal(false)
+
+  useEffect(() => {
+    // TweenMax.fromTo(".showroom-col", 1.5, { y: `100%` }, { y: `0%` })
+    TweenMax.fromTo(".showroom-col", 2, { autoAlpha: 0 }, { autoAlpha: 1 })
+  })
+
   return (
     <Layout>
       <SEO title="Showroom" />
       <Container className="showroom-container">
-        <div>SHOWROOM</div>
+        {data.Showroom.edges.map(images => (
+          <Col
+            lg={4}
+            key={images.node.id}
+            className="showroom-col"
+            onClick={() => {
+              setShowImageModal(true)
+              setImage(images)
+            }}
+          >
+            <Img
+              loading="eager"
+              fluid={images.node.childImageSharp.fluid}
+              alt={images.node.base}
+            />
+          </Col>
+        ))}
+        <Modal show={showImageModal} onHide={handleClose} size="lg" centered>
+          <Modal.Body>
+            {" "}
+            <button
+              onClick={() => setShowImageModal(false)}
+              className="showroom-btn"
+            >
+              X
+            </button>
+          </Modal.Body>
+          {showImageModal && (
+            <Img
+              loading="eager"
+              fluid={image.node.childImageSharp.fluid}
+              alt={image.node.base}
+              className="image-modal"
+            />
+          )}
+        </Modal>
       </Container>
     </Layout>
   )
 }
 
 export default Showroom
+
+export const pageQuery = graphql`
+  query {
+    Showroom: allFile(
+      filter: { relativeDirectory: { eq: "Showroom" } }
+      sort: { fields: base, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          base
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp_noBase64
+            }
+          }
+        }
+      }
+    }
+  }
+`
