0f5a9aa68f3917222a87ad030d1b3555c642099f
Alex <alex.andrul10@gmail.com>
2020-11-13 20:42:11 +0000

    add projects

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,6 +1,18 @@
-describe(`Render home page`, () => {
+describe("Home", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
   it(`renders the navbar`, () => {
-    cy.visit(`/`)
     cy.contains(/birou/i).click()
+  })
+
+  it("accepts input", () => {
+    cy.contains(/contact/i).click()
+    const inputName = "Enter name"
+    const inputEmail = "Email"
+    cy.get('[name="name"]').type(inputName).should("have.value", inputName)
+    cy.get('[name="email"]').type(inputEmail).should("have.value", inputEmail)
+    cy.get(".contact-button").click()
   })
 })
